f181f17127650fd5f09d6f12658fd9489901b22e
Jantu Deb <jantud54@gmail.com>
2022-03-20 13:40:10 +0530

    added ui for cart component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/navbar/Navbar";
 import Products from "./pages/Products";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/products" element={<Products/>}/>
+        <Route path="/cart" element={<Cart/>}/>
       </Routes>
     </div>
   );
