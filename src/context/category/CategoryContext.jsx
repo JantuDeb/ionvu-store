3f476dd71a9fed8f72827c504f55d59334854176
Jantu Deb <jantud54@gmail.com>
2022-03-23 21:37:14 +0530

    resolved merge conflict

--- src/context/category/CategoryContext.jsx
+++ src/context/category/CategoryContext.jsx
@@ -3,7 +3,7 @@ import { axiosInstance } from "../../utils/axios-instance";
 
 const CategoryContext = createContext([]);
 const CategoryProvider = ({ children }) => {
-  const [categories, setCategories] = useState();
+  const [categories, setCategories] = useState([]);
   const [status, setStatus] = useState({ loading: true, error: "" });
 
   const loadCategory = async () => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 // import Banner from "../components/home/banner/Banner";
 import Categories from "../components/home/category/Categories";
+import { CategoryProvider } from "../context/category/CategoryContext";
 // import Trending from "../components/home/trending/Trending";
 
 const Home = () => {
   return (
-    <>
+    <CategoryProvider>
       <Categories />
       {/* <Banner /> */}
       {/* <Trending /> */}
-    </>
+    </CategoryProvider>
   );
 };
 
