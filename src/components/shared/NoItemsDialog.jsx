5bfa5b55446621266ce40aadaf998a50939b0f7a
Jantu Deb <jantud54@gmail.com>
2022-03-25 09:09:54 +0530

    fix: import fix after merge

--- src/components/shared/NoItemsDialog.jsx
+++ src/components/shared/NoItemsDialog.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const NoItemsDialog = ({ children, title, description }) => {
   return (
     <div className="flex-col center bg-white my-6 p-2">
-      <img src="./assets/images/cart.webp" className="img-fluid cart-item-0" />
+      <img src="./assets/images/cart.webp" alt="no items found" className="img-fluid cart-item-0" />
       <h6>{title}</h6>
       <p className="text-center">{description} </p>
       {children}
